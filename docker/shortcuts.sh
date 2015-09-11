9cc80867f41cb81a503f008d34106bc1df84098d
Nathan Sparks <nsparks@jlab.org>
2015-09-11 19:04:58 -0400

    Add "dockerfiles" for building sim-recon in Linux containers
    The builds are split into three files; first, build the base dependencies,
    starting from the CentOS 6/7, Fedora 22, or Ubuntu 14 official image. Next,
    build the listed dependencies of sim-recon. Finally, build the master
    branches of hdds and sim-recon against those dependencies.
    
    The prerequisite packages specified in the "Dockerfile-base" files are taken
    from the gluex_install scripts written by Mark Ito. In some cases additional
    packages were required because these Linux distribution images only contain
    the core packages.

---
+++ docker/shortcuts.sh
@@ -0,0 +1,10 @@
+# some bash aliases and functions to use with docker
+alias docker-ip="docker inspect --format {{.NetworkSettings.IPAddress}}"
+docker-rm() { docker rm $(docker ps -aq); }
+#docker-rmi() { docker rmi $(docker images -q); }
+alias docker-run="docker run -it --rm"
+DATA_DIR=/path/to/data/directory
+CCDB_USER=$USER
+dock() { docker run -it -v $DATA_DIR:/home/work/data sim-recon \
+    /bin/sh -c "source /home/work/hdpm/pkgs/env-setup/hdenv.sh &&
+    export CCDB_USER=$CCDB_USER && $1"; }
