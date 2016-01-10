837c3e242db694425cc0c482f1c2560901a29e03
Nathan Sparks <nsparks@jlab.org>
2016-01-10 14:27:20 -0500

    Set DATA_DIR to default setting in shortcuts.sh

--- docker/shortcuts.sh
+++ docker/shortcuts.sh
@@ -3,7 +3,7 @@ alias docker-ip="docker inspect --format {{.NetworkSettings.IPAddress}}"
 docker-rm() { docker rm $(docker ps -aq); }
 #docker-rmi() { docker rmi $(docker images -q); }
 alias docker-run="docker run -it --rm"
-DATA_DIR=/path/to/data/directory
+DATA_DIR=`pwd`/data
 dock() { docker run -it --rm -v $DATA_DIR:/home/hdpm/docker/data sim-recon \
     /bin/bash -c "source /home/hdpm/pkgs/env-setup/hdenv.sh &&
     export CCDB_USER=$USER && $1"; }
