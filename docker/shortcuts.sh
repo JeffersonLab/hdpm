16d2185b4d77088c4d7b40c3ace550654a9dc1f1
Nathan Sparks <nsparks@jlab.org>
2016-01-10 13:36:37 -0500

    Remove the container after it exits in dock()

--- docker/shortcuts.sh
+++ docker/shortcuts.sh
@@ -4,5 +4,6 @@ docker-rm() { docker rm $(docker ps -aq); }
 #docker-rmi() { docker rmi $(docker images -q); }
 alias docker-run="docker run -it --rm"
 DATA_DIR=/path/to/data/directory
-dock() { docker run -it -v $DATA_DIR:/home/hdpm/docker/data sim-recon \
-    /bin/bash -c "source /home/hdpm/pkgs/env-setup/hdenv.sh && $1"; }
+dock() { docker run -it --rm -v $DATA_DIR:/home/hdpm/docker/data sim-recon \
+    /bin/bash -c "source /home/hdpm/pkgs/env-setup/hdenv.sh &&
+    export CCDB_USER=$USER && $1"; }
