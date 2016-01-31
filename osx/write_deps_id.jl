50a86362e207d02a758654ce8431b6f9974f59f6
Nathan Sparks <nsparks@jlab.org>
2016-01-31 13:10:17 -0500

    Use jana build timestamp as deps id for Mac OS X

--- osx/write_deps_id.jl
+++ osx/write_deps_id.jl
@@ -1,6 +1,7 @@
 top="../pkgs/osx"
-dir = filter(r"root-.+",readdir(top))[1]
-s = split(split(readall("$top/$dir/success.hdpm"))[2],"-")
+os = readchomp(`perl ../src/osrelease.pl`)
+dir = filter(r"jana-.+",readdir(top))[1]
+s = split(split(readall("$top/$dir/$os/success.hdpm"))[2],"-")
 id_deps = string(s[1][4],s[2],s[3][1:2])
 f = open(joinpath(pwd(),".id-deps-osx"),"w")
 write(f,id_deps)
