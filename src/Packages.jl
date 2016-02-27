6c718bf90ae28e67b723afdb8346d86edc08820e
Nathan Sparks <nsparks@jlab.org>
2016-02-26 19:23:34 -0500

    Fix pathname for RHEL builds against CentOS dist.

--- src/Packages.jl
+++ src/Packages.jl
@@ -354,6 +354,7 @@ function check_deps(pkg)
                 p0 = path(pkg_ld)
                 if contains(p0,jlab_top()) || !ispath(p0) continue end
                 p = (name(pkg_ld)=="amptools") ?  p0 : joinpath(p0,osrelease())
+                if contains(osrelease(),"RHEL") && contains(p0,"/.dist/") p = replace(p,"RHEL","CentOS") end
                 record = split(readall("$p/success.hdpm"))[end]
                 if !contains(record,name_ver) error("$name_ver is incompatible with $user_name_ver.\n$user_name_ver depends on $record.\nRebuild $user_name_ver against $name_ver, or use required $(name(pkg_shlib)) version.\n") end
             end
