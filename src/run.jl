e016a20afa9e843da13083126571d56f561036df
Nathan Sparks <nsparks@jlab.org>
2016-03-26 22:36:27 -0400

    run: Don't change to default work directory

--- src/run.jl
+++ src/run.jl
@@ -2,8 +2,6 @@ using Environs,Packages
 # run a command in the Hall-D offline environment
 @osx_only usage_error("The 'hdpm run' command is not supported on Mac OS X.")
 printenv() # expose env variables and save them to env-setup file
-ts = readchomp(`date "+%Y-%m-%d"`)
-mk_cd("work/ByDate/$ts")
 ENV["CCDB_USER"] = ENV["USER"]
 if length(ARGS) > 1 error("'hdpm run' does not support multiple arguments. \n\t\t\tPut multi-word commands in double quotes.") end
 cmd = (length(ARGS) == 1) ? ARGS[1] : "bash"
