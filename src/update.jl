5fe8ebfbc3db111450df6374595b6615bc4b7088
Nathan Sparks <nsparks@jlab.org>
2016-06-08 12:06:59 -0400

    update: Skip a package if its path does not exist

--- src/update.jl
+++ src/update.jl
@@ -2,7 +2,7 @@ using Packages
 # update svn and git packages
 for pkg in get_packages(); if length(ARGS) > 0 if !(name(pkg) in ARGS) continue end end
     if contains(url(pkg),"svn") && !contains(url(pkg),"tags") && !is_external(pkg)
-        if !ispath(path(pkg)) usage_error(path(pkg)," does not exist.\n\tRun 'hdpm build'.") end
+        if !ispath(path(pkg)) continue end
         cd(path(pkg))
         rev = version(pkg)
         println(string("Updating ",name(pkg)," to svn revision $rev."))
@@ -13,7 +13,7 @@ for pkg in get_packages(); if length(ARGS) > 0 if !(name(pkg) in ARGS) continue 
         end
     end
     if contains(url(pkg),"git") && !contains(url(pkg),"archive") && !is_external(pkg)
-        if !ispath(path(pkg)) usage_error(path(pkg)," does not exist.\n\tRun 'hdpm build'.") end
+        if !ispath(path(pkg)) continue end
         cd(path(pkg))
         branch = version(pkg)
         println("Updating $branch branch of ",name(pkg),".")
